008c1db31dedb490c1f861472564b5bdc92a8053
-(--- <2968640488@qq.com>
2022-01-06 19:09:38 +0800

    UPDT(RHI): 封装了两个OpenGL的函数

--- Include/RHI/RHI.h
+++ Include/RHI/RHI.h
@@ -3,11 +3,38 @@
 
 #include <AnciCore.h>
 
+#ifndef AnciVtxBuffer
+  typedef void * AnciVtxBuffer;
+#endif
+
+#ifndef AnciIdxBuffer
+  typedef void * AnciIdxBuffer;
+#endif
+
+/**
+ * RHI函数指针，命名规范：
+ *      1. typedef <Result> <ANCI_RHI_*>();
+ *      2. 定义导出函数: ANCIAPI <ANCI_RHI_*> <变量名去掉函数指针命名的下划线>
+ *      3. 定义宏，使用函数
+ *
+ * 示例：
+ *      typedef void (*ANCI_RHI_TEST)();
+ *      ANCIAPI ANCI_RHI_TEST ANCIRHITEST;
+ *      #define RHITest ANCIRHITEST
+ */
 typedef void (*ANCI_RHI_VIEWPORT)(anciu32 x, anciu32 y, anciu32 w, anciu32 h);
 ANCIAPI ANCI_RHI_VIEWPORT ANCIRHIVIEWPORT;
 #define RHIViewport ANCIRHIVIEWPORT
 typedef void (*ANCI_RHI_SWAP_BUFFERS)(ANCI_WINDOW_HANDLE h);
 ANCIAPI ANCI_RHI_SWAP_BUFFERS ANCIRHISWAPBUFFERS;
 #define RHISwapBuffers ANCIRHISWAPBUFFERS
-
+typedef void (*ANCI_RHI_GEN_VTXBUFFER)();
+ANCIAPI ANCI_RHI_GEN_VTXBUFFER ANCIRHIGENVTXBUFFER;
+#define RHIGenVtxBuffer ANCIRHIGENVTXBUFFER
+typedef void (*ANCI_RHI_GEN_IDXBUFFER)();
+ANCIAPI ANCI_RHI_GEN_IDXBUFFER ANCIRHIGENIDXBUFFER;
+#define RHIGenIdxBuffer ANCIRHIGENIDXBUFFER
+/**
+ * 加载对应的API函数
+ */
 ANCIAPI void AnciLoadRHI();
