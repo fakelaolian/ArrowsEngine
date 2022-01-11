e6d911e401a064f3346538f686e6f2f1fedf7fe1
-(--- <2968640488@qq.com>
2022-01-11 11:34:25 +0800

    FEAT: 添加万恶的ImGui

--- Engine/Application/EngineApplication.cpp
+++ Engine/Application/EngineApplication.cpp
@@ -5,8 +5,43 @@
 #include <RHI/RHI.h>
 #define STB_IMAGE_IMPLEMENTATION
 #include <stb_image.h>
+#include <GLFW/glfw3.h>
 
 AnciEnumGraphicsApi g_AnciGraphicsApi;
+
+#include <imgui/imgui.h>
+#include <imgui/backends/imgui_impl_glfw.h>
+#include <imgui/backends/imgui_impl_opengl3.h>
+
+void SetUpImGui(ANCI_WINDOW_HANDLE h)
+{
+        // Setup Dear ImGui context
+        IMGUI_CHECKVERSION();
+        ImGui::CreateContext();
+        ImGuiIO& io = ImGui::GetIO(); (void)io;
+        io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;       // Enable Keyboard Controls
+        //io.ConfigFlags |= ImGuiConfigFlags_NavEnableGamepad;      // Enable Gamepad Controls
+        io.ConfigFlags |= ImGuiConfigFlags_DockingEnable;           // Enable Docking
+        io.ConfigFlags |= ImGuiConfigFlags_ViewportsEnable;         // Enable Multi-Viewport / Platform Windows
+        //io.ConfigViewportsNoAutoMerge = true;
+        //io.ConfigViewportsNoTaskBarIcon = true;
+
+        // Setup Dear ImGui style
+        ImGui::StyleColorsDark();
+        //ImGui::StyleColorsClassic();
+
+        // When viewports are enabled we tweak WindowRounding/WindowBg so platform windows can look identical to regular ones.
+        ImGuiStyle& style = ImGui::GetStyle();
+        if (io.ConfigFlags & ImGuiConfigFlags_ViewportsEnable)
+        {
+                style.WindowRounding = 0.0f;
+                style.Colors[ImGuiCol_WindowBg].w = 1.0f;
+        }
+
+        // Setup Platform/Renderer backends
+        ImGui_ImplGlfw_InitForOpenGL((GLFWwindow *) h, true);
+        ImGui_ImplOpenGL3_Init("#version 410");
+}
 
 void FResizeCallback(anciu32 x, anciu32 y)
 {
@@ -22,10 +57,13 @@ EngineApplication::EngineApplication(AnciEnumGraphicsApi api)
 
         /* 初始化RHI函数 */
         RHIApiLoad();
+
+        SetUpImGui(_window->GetHandle());
 }
 
 void EngineApplication::StartEngine()
 {
+
         RHIVtxArray vertices[] = {
                 {{0.5f,  0.5f,  0.0f}, {1.0f, 0.5f, 0.2f}, {1.0f, 1.0f}},
                 {{0.5f,  -0.5f, 0.0f}, {1.0f, 0.5f, 0.2f}, {1.0f, 0.0f}},
@@ -62,27 +100,61 @@ void EngineApplication::StartEngine()
         shader->SetInt("ourSampler2D_0", 0);
         shader->SetInt("ourSampler2D_1", 1);
 
-        ancimat4 model{1.0f};
-        ancimat4 projection{1.0f};
-        ancimat4 view{1.0f};
-        model = glm::rotate(model, glm::radians(-55.0f), glm::vec3(1.0f, 0.0f, 0.0f));
-        view = glm::translate(view, glm::vec3(0.0f, 0.0f, -3.0f));
-        ancivec2 dimension = _window->GetDimension();
-        projection = glm::perspective(glm::radians(45.0f), (float) dimension.x / (float) dimension.y, 0.1f, 100.0f);
-
-        shader->SetMat4("model", model);
-        shader->SetMat4("view", view);
-        shader->SetMat4("proj", projection);
+        bool show_demo_window = false;
+        float zNear = 0.01f, zFar = 100.0f;
 
         while (!_window->ShouldClose()) {
+                _window->PollEvents();
+
+                // Start the Dear ImGui frame
+                ImGui_ImplOpenGL3_NewFrame();
+                ImGui_ImplGlfw_NewFrame();
+                ImGui::NewFrame();
+
+                if (show_demo_window)
+                        ImGui::ShowDemoWindow(&show_demo_window);
+
                 RHIClearColorBuffer(ancivec4(0.2f));
+
+                if (ImGui::Begin("Debug")) {
+                        ImGui::Text("ZNear");
+                        ImGui::SameLine();
+                        ImGui::DragFloat("zNear", &zNear, 0.1f);
+                        ImGui::Text("zFar");
+                        ImGui::SameLine();
+                        ImGui::DragFloat("zFar", &zFar, 1.0f);
+                } ImGui::End();
+
+                ancimat4 model{1.0f};
+                ancimat4 projection{1.0f};
+                ancimat4 view{1.0f};
+                model = glm::rotate(model, glm::radians(-55.0f), glm::vec3(1.0f, 0.0f, 0.0f));
+                view = glm::translate(view, glm::vec3(0.0f, 0.0f, -3.0f));
+                ancivec2 dimension = _window->GetDimension();
+                projection = glm::perspective(glm::radians(45.0f), (float) dimension.x / (float) dimension.y, zNear, zFar);
+
+                shader->SetMat4("model", model);
+                shader->SetMat4("view", view);
+                shader->SetMat4("proj", projection);
 
                 RHIBindTexture(texture0);
                 RHIBindTexture(texture1);
                 RHIBindVtxBuffer(vtxBuffer);
                 RHIDrawIdx(idxBuffer);
 
+                // Rendering
+                ImGui::Render();
+                ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+
+                ImGuiIO &io = ImGui::GetIO();
+                if (io.ConfigFlags & ImGuiConfigFlags_ViewportsEnable)
+                {
+                        GLFWwindow* backup_current_context = glfwGetCurrentContext();
+                        ImGui::UpdatePlatformWindows();
+                        ImGui::RenderPlatformWindowsDefault();
+                        glfwMakeContextCurrent(backup_current_context);
+                }
+
                 RHISwapBuffers(_window->GetHandle());
-                _window->PollEvents();
         }
 }
